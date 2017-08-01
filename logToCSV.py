2c5773823296a59cf25988eef1210edcb4a0685c
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-08-01 15:23:29 -0500

    added barebones support for parsing each event attribute into its own event object and culminating all of them in a log array object

--- logToCSV.py
+++ logToCSV.py
@@ -2,28 +2,78 @@ import re
 
 log = []
 numEvents = 0
-line = []
 
 with open("eventlog.txt") as file:
 	lines = file.read()
 	eventsList = re.findall("(Event\[)[0-9]+(\])", lines)
 	numEvents = len(eventsList)
 	print(numEvents)
-	print(type(lines))
 	
 with open("eventlog.txt") as file:
 	eventCount = 0
 	while eventCount < numEvents:
 		line = file.readline().strip()
-		eventsFound = re.findall("(Event\[)[0-9]+(\])", line) 
-		print(eventsFound)
-		print(line)
+		eventsFound = re.findall("(Event\[)[0-9]+(\])", line)
 		isBeginning = len(eventsFound) == 1
 		if (isBeginning):
+			event = {}
 			eventCount+= 1
+			eventNumberArray = re.findall("[0-9]+", line)
+			eventNumber = int(eventNumberArray[0])
+			event['Number'] = eventNumber
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Log Name'] = wordArray[2]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Source'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Date'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Event ID'] = wordArray[2]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Task'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Level'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Opcode'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Keyword'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['User'] = wordArray[1]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['User Name'] = wordArray[2]
+			
+			line = file.readline().strip()
+			wordArray = line.split()
+			event['Computer'] = wordArray[1]
+			
+			line = file.readline()
+			line = file.readline().strip()
+			event['Description'] = line
+			
+			log.append(event)
 		# print(isBeginning)
 		# print(line)
-
+	print(log)
 	# for line in file:
 		#stripped_line = line.rstrip()
 		# log.append(stripped_line)
