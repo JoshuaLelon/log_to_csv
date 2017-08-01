72c312ab599402255cdf33206f5cbfc72192fef6
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-08-01 14:42:05 -0500

    added sample event log

--- logToCSV.py
+++ logToCSV.py
@@ -6,12 +6,13 @@ with open("eventlog.txt") as file:
 	numEvents = len(eventsList)
 	print(numEvents)
 	log = []
-	# while True:
-    # line = file.readline().strip()
-    # if line == '':
-        # either end of file or just a blank line.....
-        # we'll assume EOF, because we don't have a choice with the while loop!
-        # break
+	while True:
+		line = file.readline().strip()
+		if line == '':
+			# either end of file or just a blank line.....
+			# we'll assume EOF, because we don't have a choice with the while loop!
+			break
+		print(line)
 
 	# for line in file:
 		#stripped_line = line.rstrip()
