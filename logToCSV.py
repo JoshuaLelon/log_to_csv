a201169cadbd5e29dedcf2000425cfb69432ebb4
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-08-07 21:08:07 -0500

    refactored event txt to python dict obj code into for loop

--- logToCSV.py
+++ logToCSV.py
@@ -7,78 +7,42 @@ eventsListFileName = "eventlog.txt"
 log = []
 numEvents = events.getNumEvents(eventsListFileName)
 	
-with open("eventlog.txt") as file:
+with open(eventsListFileName) as file:
 	eventCount = 0
+	eventAttributeDictionary = {
+		'Log Name': 2,
+		'Source': 1, 
+		'Date': 1,
+		'Event ID': 2,
+		'Task': 1,
+		'Level': 1,
+		'Opcode': 1,
+		'Keyword': 1,
+		'User': 1,
+		'User Name': 2,
+		'Computer': 1
+		}
+		
 	while eventCount < numEvents:
 		line = file.readline().strip()
 		eventsFound = re.findall("(Event\[)[0-9]+(\])", line)
-		isBeginning = len(eventsFound) == 1
-		if (isBeginning):
+		isBeginning = len(eventsFound) == 1 # if this line is the start of an event
+		if (isBeginning): # then parse it
 			event = {}
 			eventCount+= 1
 			eventNumberArray = re.findall("[0-9]+", line)
 			eventNumber = int(eventNumberArray[0])
 			event['Number'] = eventNumber
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Log Name'] = wordArray[2]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Source'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Date'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Event ID'] = wordArray[2]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Task'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Level'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Opcode'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Keyword'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['User'] = wordArray[1]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['User Name'] = wordArray[2]
-			
-			line = file.readline().strip()
-			wordArray = line.split()
-			event['Computer'] = wordArray[1]
+			for key, value in eventAttributeDictionary.items(): # this assumes it loops through the dict in the order the dict is initialized with
+				line = file.readline().strip()
+				wordArray = line.split()
+				event[key] = wordArray[value]
 			
 			line = file.readline()
 			line = file.readline().strip()
 			event['Description'] = line
 			
 			log.append(event)
-		# print(isBeginning)
-		# print(line)
-	# print(log)
-	# for line in file:
-		#stripped_line = line.rstrip()
-		# log.append(stripped_line)
-	#for line in log:
-		# print(line)
-		# print(line)
-		# print("---------------------------")
 		
 with open('result.json', 'w') as fp:
     json.dump(log, fp)
