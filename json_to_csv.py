79420b1f2d0f6418360138d3c4506be91640bca1
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-08-07 21:55:02 -0500

    Added JSON to CSV conversion module and included it in the overall log_to_csv script

---
+++ json_to_csv.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+eventHeaders = {
+				'Log Name': 2,
+				'Source': 1, 
+				'Date': 1,
+				'Event ID': 2,
+				'Task': 1,
+				'Level': 1,
+				'Opcode': 1,
+				'Keyword': 1,
+				'User': 1,
+				'User Name': 2,
+				'Computer': 1,
+				'Description': 1
+}
+
+def get_csv_from_JSON(jsonFileName, csvFileName):
+	with open(jsonFileName) as json_file:
+		eventsJSON = json.load(json_file)
+		f = csv.writer(open(csvFileName, "w"), delimiter=',', lineterminator='\n')
+
+		# f.writerow(["pk", "model", "codename", "name", "content_type"])
+		f.writerow([key for key, value in eventHeaders.items()])
+
+		for event in eventsJSON:
+			print(event)
+			# f.writerow([event["pk"],
+			#			x["model"],
+			#			x["fields"]["codename"],
+			#			x["fields"]["name"],
+			#			x["fields"]["content_type"]])
+			f.writerow([value for key, value in event.items()])
+
--- logToCSV.py
+++ logToCSV.py
@@ -1,12 +1,13 @@
-import re
-import json
-import csv
 import events
 import txt_to_py_list
 import json_dump
+import json_to_csv
 
 eventListFileName = "eventlog.txt"
 jsonResultsFileName = "results.json"
+csvFileName = "results.csv"
+
 numEvents = events.getNumEvents(eventListFileName)
 eventList = txt_to_py_list.getPyListFromTxt(eventListFileName, numEvents)
 json_dump.dumpListAsJSON(eventList, jsonResultsFileName)
+json_to_csv.get_csv_from_JSON(jsonResultsFileName, csvFileName)
